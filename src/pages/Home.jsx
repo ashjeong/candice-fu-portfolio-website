2a60503e01583769fbeda59175b9e174f783d696
ashjeong <ashjeong@umich.edu>
2026-01-14 00:21:56 +0900

    feat: impl home page hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
+import meditationBackground from '../assets/meditation-background.svg';
+import meditation from '../assets/meditation.svg';
+import "./Home.css";
+
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <h1>hi!</h1>
+        <h1>i'm candice!</h1>
+        <p>I am a passionate graphic designer and illustrator with a love for creating that brings joy to others. </p>
+      </div>
+      <div className="hero-image-container">
+        <img
+          className="hero-image hero-background"
+          src={meditationBackground}
+          alt="Meditation Background"
+        />
+        <img
+          className="hero-image hero-meditation"
+          src={meditation}
+          alt="Meditation Character"
+        />
+      </div>
+    </div>
+  );
+}
 
 function App() {
 
   return (
     <>
-      Home Page content
+      <Hero />
     </>
   );
 }
