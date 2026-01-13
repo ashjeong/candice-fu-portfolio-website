6dcca6fe60e3bb28e709e85aa4d7ba25c9feba02
ashjeong <ashjeong@umich.edu>
2026-01-14 00:22:23 +0900

    feat: impl about page hero

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,31 @@
+import aboutDrawing from '../assets/about-drawing.png';
+import "./Home.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <h1>hi!</h1>
+        <h1>i'm candice!</h1>
+        <p>I am a passionate graphic designer and illustrator with a love for creating that brings joy to others. </p>
+      </div>
+      <div className="hero-image-container">
+        <img
+          className="hero-image hero-background"
+          src={aboutDrawing}
+          alt="About Drawing"
+        />
+      </div>
+    </div>
+  );
+}
+
 
 function About() {
 
   return (
     <>
-      About Content
+      <Hero />
     </>
   );
 }
