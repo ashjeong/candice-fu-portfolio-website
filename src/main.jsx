f6d12ae44ec07003f154c6cebd8191e42c4a4ccc
ashjeong <ashjeong@umich.edu>
2026-01-16 20:31:33 +0900

    feat: add home page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import MainLayout from './layouts/MainLayout';
+import Home from './pages/home/Home';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
