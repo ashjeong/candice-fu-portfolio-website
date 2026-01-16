707b6eadbf72633ce68136170f9910a76e6f3f0a
ashjeong <ashjeong@umich.edu>
2026-01-16 20:20:13 +0900

    feat: create `Header` component

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -2,23 +2,23 @@ import { Link, Stack, useMediaQuery } from "@mui/material";
 import "./NavLinks.css";
 
 
-export default function NavLinks() {
+export default function NavLinks({ isDrawer = false }) {
   const isSmallScreen = useMediaQuery("(max-width:600px)");
 
   return (
     <Stack
       direction={isSmallScreen ? "column" : "row"}
       spacing={isSmallScreen ? 3 : { sm: 2, md: 4 }}
-      className={"links"}
+      className={isDrawer ? "links-drawer" : "links"}
     >
-      <Link class={"link"} href="/work" underline="none">
-        {"WORK"}
+      <Link class={isDrawer ? "link-drawer" : "link"} href="/work" underline="none">
+        {isDrawer ? "Work" : "WORK"}
       </Link>
-      <Link class={"link"} href="/about" underline="none">
-        {"ABOUT"}
+      <Link class={isDrawer ? "link-drawer" : "link"} href="/about" underline="none">
+        {isDrawer ? "About" : "ABOUT"}
       </Link>
-      <Link class={"link"} href="https://drive.google.com/file/d/18b9roccNMzs-1yWNJCMEHDjkmZakp_pQ/view?pli=1" target="_blank" underline="none">
-        {"RESUME"}
+      <Link class={isDrawer ? "link-drawer" : "link"} href="https://drive.google.com/file/d/18b9roccNMzs-1yWNJCMEHDjkmZakp_pQ/view?pli=1" target="_blank" underline="none">
+        {isDrawer ? "Resume" : "RESUME"}
       </Link>
     </Stack>
   );
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Stack } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header.jsx';
 
 
 export default function MainLayout() {
 
   return (
     <Stack sx={{ minHeight: "100vh" }}>
+      <Header />
       <main style={{ flex: "auto" }}>
         <Outlet />
       </main>
