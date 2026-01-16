fd5678b7fb0480daca0c45e588d4b1af732fbf09
ashjeong <ashjeong@umich.edu>
2026-01-16 20:29:28 +0900

    feat: create base setup

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
