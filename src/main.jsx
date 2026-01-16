ea9ef0fe4d19cde2b38ff67a93c7fdee9282bec1
ashjeong <ashjeong@umich.edu>
2026-01-16 20:31:25 +0900

    feat: add about page

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import MainLayout from './layouts/MainLayout';
+import About from './pages/about/About';
 import Home from './pages/home/Home';
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,55 @@
+import "./About.css";
+import aboutDrawing from './assets/about-drawing.png';
+import pfpCandice from './assets/pfp-candice.jpg';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img
+          className="hero-image about-hero-image"
+          src={aboutDrawing}
+          alt="About Drawing"
+        />
+      </div>
+      <div className="hero-text">
+        <h2>hi, i'm candice!</h2>
+        <p>I am a passionate graphic designer and illustrator with a love for creating meaningful and visually engaging work that brings joy to others. </p>
+      </div>
+    </div>
+  );
+}
+
+function AboutContent() {
+  return (
+    <div className="hero">
+      <div>
+        <img
+          className="hero-image"
+          style={{
+            borderRadius: "50%"
+          }}
+          src={pfpCandice}
+          alt="Profile Picture of Candice Fu"
+        />
+      </div>
+      <div className="hero-text" >
+        <p className="p2">I graduated from the Rochester Institute of Technology in 2023 with a BFA in Graphic Design and a minor in Advertising and Public Relations. Currently, I am working as a junior designer at Arnold NY, where I have the opportunity to further refine my design skills and contribute to impactful projects. </p>
+        <p className="p2">As a designer, I’m constantly striving to push my creative boundaries and produce work of the highest quality. I enjoy exploring various aspects of design to continually improve my craft and expand my capabilities. In my personal projects, I like to add a bit of fun and humor, bringing a unique and playful perspective to my designs!</p>
+      </div>
+    </div>
+  );
+}
+
+
+function About() {
+
+  return (
+    <>
+      <Hero />
+      <AboutContent />
+    </>
+  );
+}
+
+export default About;
