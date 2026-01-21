7095529e9d7943580be2e0e064f7312a4043d25d
Skrai Pardus <84874402+ashjeong@users.noreply.github.com>
2026-01-22 02:02:21 +0900

    feat: impl base pages (#1)
    * feat: create base setup
    
    * feat: add setup dependencies
    
    * docs: add run directions to readme
    
    * feat: create main layout
    
    * feat: add `LogoIcon` component
    
    * feat: create `NavLinks` component
    
    * feat: create `Header` component
    
    * feat: add header scroll transition
    
    * feat: create `Footer` component
    
    * feat: add home page
    
    * feat: add about page
    
    * feat: create `WorkCard` component
    
    TODO: sizing must be fixed!
    
    * feat: add work page
    
    * chore: move components into folders

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,54 @@
+import { CloseRounded, MenuRounded } from "@mui/icons-material";
+import { Drawer, Link, useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import "./Header.css";
+import LogoIcon from "../common/Logo";
+import NavLinks from "../navlinks/NavLinks";
+
+
+export default function Header({ showHeader }) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
+
+  return (
+    <div className="header" style={{ transform: showHeader ? "translateY(0)" : "translateY(-120%)" }}>
+      <Link href="/">
+        <LogoIcon style={{ width: "4rem", height: "auto", fill: "#FF5115" }} />
+      </Link>
+      {isSmallScreen ? (
+        <>
+          <MenuRounded
+            aria-label="open drawer"
+            onClick={() => {
+              setDrawerOpen(true);
+            }}
+            sx={{ color: "#212121", fontSize: "5rem", fontWeight: "normal" }}
+          />
+          <Drawer
+            anchor="right"
+            open={drawerOpen}
+            PaperProps={{ sx: { width: "100vw", height: "100vh", backgroundColor: "#FFF8EE" } }}
+          >
+            <CloseRounded
+              onClick={() => {
+                setDrawerOpen(false);
+              }}
+              aria-label="close drawer"
+              className="close-drawer"
+              sx={{
+                fontSize: "5rem",
+                position: "absolute",
+                top: "1.5rem",
+                right: "1.5rem",
+                color: "#212121"
+              }}
+            />
+            <NavLinks isDrawer={true} />
+          </Drawer>
+        </>
+      ) : (
+        <NavLinks />
+      )}
+    </div>
+  );
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,38 @@
+import meditationBackground from './assets/meditation-background.svg';
+import meditation from './assets/meditation.svg';
+import "./Home.css";
+
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div style={{ position: "relative" }}>
+        <img
+          className="hero-image"
+          style={{ aspectRatio: "629/517" }}
+          src={meditationBackground}
+          alt="Meditation Background"
+        />
+        <img
+          className="hero-image meditation"
+          src={meditation}
+          alt="Meditation Character"
+        />
+      </div>
+      <div className="hero-text">
+        <h1>hi!</h1>
+        <h1>i’m candice!</h1>
+        <p>I am a passionate graphic designer and illustrator with a love for creating that brings joy to others.</p>
+      </div>
+    </div>
+  );
+}
+
+export default function Home() {
+
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
