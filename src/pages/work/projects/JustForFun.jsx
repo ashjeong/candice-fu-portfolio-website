6cf9b14a8cf8fea1d1337fc927c6796dd10a7bb7
maggoles <maggoles@umich.edu>
2026-01-26 23:33:40 -0500

    feat(work): added work page scaffolding

---
+++ src/pages/work/projects/JustForFun.jsx
@@ -0,0 +1,8 @@
+export default function JustForFun() {
+  return (
+    <div>
+      <h1>Just For Fun Project Page</h1>
+      <p>This is the detailed page for the Just For Fun project.</p>
+    </div>
+  );
+}
